981ca3991846e6f3553b9655e60effa01a04d18a
Michael Krog <mic@apaq.dk>
2019-05-12 20:22:40 +0200

    1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,70 @@
 interface IContact {
-    id: string;
+  id: string;
 }
 
 interface IAgreement {
-    id: string;
+  id: string;
 }
 
 interface IAddress {
-    city: string;
-    countryCode: string;
-    houseNumber: string;
-    latitude: string;
-    longitude: string;
-    postalCode: string;
-    street: string;
-    village: string;
+  city: string;
+  countryCode: string;
+  houseNumber: string;
+  latitude: string;
+  longitude: string;
+  postalCode: string;
+  street: string;
+  village: string;
 }
 
 class Previsto {
-    public apiKey: string = '';
-    private serviceUrl = 'https://api.previsto.io';
+  public apiKey: string = '';
+  private serviceUrl = 'https://api.previsto.io';
 
-    public async createContact(contact: IContact, twoFaToken: string): Promise<IContact> {
-        return fetch(`${this.serviceUrl}/contacts`, {
-            body: JSON.stringify(contact),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'X-2FA-Token': twoFaToken,
-                'X-Auth-Token': this.apiKey
-            },
-            method: 'POST'
-        }).then(res => res.json())
-            .then(res => res.map((result: IContact) => result));
-    }
+  public async createContact(contact: IContact, twoFaToken: string): Promise<IContact> {
+    return fetch(`${this.serviceUrl}/contacts`, {
+      body: JSON.stringify(contact),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-2FA-Token': twoFaToken,
+        'X-Auth-Token': this.apiKey,
+      },
+      method: 'POST',
+    })
+      .then(res => res.json())
+      .then(res => res.map((result: IContact) => result));
+  }
 
-    public async createAgreement(agreement: IAgreement, twoFaToken: string): Promise<IAgreement> {
-        return fetch(`${this.serviceUrl}/agreements`, {
-            body: JSON.stringify(agreement),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'X-2FA-Token': twoFaToken,
-                'X-Auth-Token': this.apiKey
-            },
-            method: 'POST'
-        }).then(res => res.json())
-            .then(res => res.map((result: IAgreement) => result));
-    }
+  public async createAgreement(agreement: IAgreement, twoFaToken: string): Promise<IAgreement> {
+    return fetch(`${this.serviceUrl}/agreements`, {
+      body: JSON.stringify(agreement),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-2FA-Token': twoFaToken,
+        'X-Auth-Token': this.apiKey,
+      },
+      method: 'POST',
+    })
+      .then(res => res.json())
+      .then(res => res.map((result: IAgreement) => result));
+  }
 
-    public async requestTwoFaToken(phone: string) {
-        return fetch(`${this.serviceUrl}/accounts/actions/sendTwoFactorToken`, {
-            body: JSON.stringify({ phone }),
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Auth-Token': this.apiKey
-            },
-            method: 'POST'
-        }).then(res => res.json());
-    }
+  public async requestTwoFaToken(phone: string) {
+    return fetch(`${this.serviceUrl}/accounts/actions/sendTwoFactorToken`, {
+      body: JSON.stringify({ phone }),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Auth-Token': this.apiKey,
+      },
+      method: 'POST',
+    }).then(res => res.json());
+  }
 
-    public async searchAddress(query: string): Promise<IAddress[]> {
-        return [];
-    }
+  public async searchAddress(query: string): Promise<IAddress[]> {
+    return [];
+  }
 }
 
 export const previsto = new Previsto();

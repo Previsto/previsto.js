6fe5905adbf7598e229645712bf666f36d2e9e1a
Michael Krog <mic@apaq.dk>
2019-05-12 19:49:28 +0200

    Adds some initial methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-class Previsto {}
+
+interface IContact {
+
+}
+
+interface IAgreement {
+
+}
+
+interface IAddress {
+
+}
+
+class Previsto {
+
+    async createContact(contact: IContact, twoFaToken?: string): Promise<IContact> {
+        return {};
+    }
+
+    async createAgreement(agreement: IAgreement, twoFaToken?: string): Promise<IAgreement> {
+        return {};
+    }
+
+    async requestTwoFaToken(phone: string) {
+
+    }
+
+    async searchAddress(query: string): Promise<IAddress[]> {
+        return [];
+    }
+}
 
 export const previsto = new Previsto();

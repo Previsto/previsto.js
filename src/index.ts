b5eb7bd97da02f9934063a67363bb53cc22de797
Michael Krog <mic@apaq.dk>
2019-05-12 19:53:37 +0200

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,42 @@
-
 interface IContact {
-    id: string;
+  id: string;
 }
 
 interface IAgreement {
-    id: string;
+  id: string;
 }
 
 interface IAddress {
-    city: string;
-    countryCode: string;
-    houseNumber: string;
-    latitude: string;
-    longitude: string;
-    postalCode: string;
-    street: string;
-    village: string;
+  city: string;
+  countryCode: string;
+  houseNumber: string;
+  latitude: string;
+  longitude: string;
+  postalCode: string;
+  street: string;
+  village: string;
 }
 
 class Previsto {
+  public async createContact(contact: IContact, twoFaToken?: string): Promise<IContact> {
+    return {
+      id: '',
+    };
+  }
 
-    public async createContact(contact: IContact, twoFaToken?: string): Promise<IContact> {
-        return {
-            id: ''
-        };
-    }
+  public async createAgreement(agreement: IAgreement, twoFaToken?: string): Promise<IAgreement> {
+    return {
+      id: '',
+    };
+  }
 
-    public async createAgreement(agreement: IAgreement, twoFaToken?: string): Promise<IAgreement> {
-        return {
-            id: ''
-        };
-    }
+  public async requestTwoFaToken(phone: string) {
+    console.log('Requests token');
+  }
 
-    public async requestTwoFaToken(phone: string) {
-        console.log('Requests token');
-    }
-
-    public async searchAddress(query: string): Promise<IAddress[]> {
-        return [];
-    }
+  public async searchAddress(query: string): Promise<IAddress[]> {
+    return [];
+  }
 }
 
 export const previsto = new Previsto();
